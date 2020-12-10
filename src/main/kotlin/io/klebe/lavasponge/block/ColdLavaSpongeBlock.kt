75b5c8dd53477bee16cb3149e80bbcf2f9d37b1e
Fabian Rehhauße <f.rehhausse@klebe.io>
2020-12-10 17:42:05 +0100

    Add cold and wet lava sponge block

---
+++ src/main/kotlin/io/klebe/lavasponge/block/ColdLavaSpongeBlock.kt
@@ -0,0 +1,12 @@
+package io.klebe.lavasponge.block
+
+import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
+import net.minecraft.block.Block
+import net.minecraft.block.Material
+import net.minecraft.sound.BlockSoundGroup
+
+object ColdLavaSpongeBlock : Block(
+    FabricBlockSettings.of(Material.STONE)
+        .requiresTool()
+        .strength(20.0F, 1200.0F)
+        .sounds(BlockSoundGroup.GRASS)) {}
---
+++ src/main/kotlin/io/klebe/lavasponge/block/WetLavaSpongeBlock.kt
@@ -0,0 +1,40 @@
+package io.klebe.lavasponge.block
+
+import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
+import net.minecraft.block.Block
+import net.minecraft.block.BlockState
+import net.minecraft.block.Blocks
+import net.minecraft.block.Material
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.sound.BlockSoundGroup
+import net.minecraft.sound.SoundCategory
+import net.minecraft.sound.SoundEvents
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
+
+object WetLavaSpongeBlock : Block(
+    FabricBlockSettings.of(Material.SPONGE)
+        .strength(1F)
+        .sounds(BlockSoundGroup.STONE)) {
+
+    override fun onBlockAdded(
+        state: BlockState?,
+        world: World,
+        pos: BlockPos?,
+        oldState: BlockState?,
+        notify: Boolean
+    ) {
+        if (!world.dimension.isUltrawarm) {
+            world.setBlockState(pos, ColdLavaSpongeBlock.defaultState, 3)
+            world.syncWorldEvent(2009, pos, 0)
+            world.playSound(
+                null,
+                pos,
+                SoundEvents.BLOCK_FIRE_EXTINGUISH,
+                SoundCategory.BLOCKS,
+                1.0f,
+                (1.0f + world.getRandom().nextFloat() * 0.2f) * 0.7f
+            )
+        }
+    }
+}
