a3afbe1b5d5be6f9fa97263ac7fd83bc4fceba53
Fabian Rehhauße <f.rehhausse@klebe.io>
2020-12-10 17:52:00 +0100

    Register all blocks

--- src/main/kotlin/io/klebe/lavasponge/LavaSponge.kt
+++ src/main/kotlin/io/klebe/lavasponge/LavaSponge.kt
@@ -1,8 +1,11 @@
 package io.klebe.lavasponge
 
+import io.klebe.lavasponge.block.ColdLavaSpongeBlock
 import io.klebe.lavasponge.block.LavaSpongeBlock
+import io.klebe.lavasponge.block.WetLavaSpongeBlock
 import kotlinx.coroutines.runBlocking
 import net.fabricmc.api.ModInitializer
+import net.minecraft.block.Block
 import net.minecraft.item.BlockItem
 import net.minecraft.item.Item
 import net.minecraft.util.Identifier
@@ -15,11 +18,17 @@ object LavaSponge : ModInitializer {
 
     val log = LogManager.getLogger(MODID)
 
+    private fun registerBlock(block: Block, id: String) {
+        Registry.register(Registry.BLOCK, Identifier(MODID, id), block)
+        Registry.register(Registry.ITEM, Identifier(MODID, id), BlockItem(block, Item.Settings()))
+    }
+
     override fun onInitialize() = runBlocking {
         log.info("Start initialization")
 
-        Registry.register(Registry.BLOCK, Identifier(MODID, "lavasponge"), LavaSpongeBlock)
-        Registry.register(Registry.ITEM, Identifier(MODID, "lavasponge"), BlockItem(LavaSpongeBlock, Item.Settings()))
+        registerBlock(LavaSpongeBlock, "lavasponge")
+        registerBlock(WetLavaSpongeBlock, "wetlavasponge")
+        registerBlock(ColdLavaSpongeBlock, "coldlavasponge")
 
         log.info("Initialization completed")
     }
